3dc10c25bee83582fe64cc3cb4af908defea8292
TerryDover <bigterkc@hotmail.com>
2021-01-14 01:14:42 -0600

    rename

--- mongo.js
+++ mongo.js
@@ -1,3 +1,19 @@
+/*
+
+Insert 3 new recipes and their ingredients, amounts, and measures in to the Database
+	
+Write a query to get a list of all the Recipes
+	
+Write a query to get a list of Recipes that use a certain Ingredient
+	
+Write a query to update a recipe you added
+		
+Put your SQL commands in a file in a Github repo and submit the repo link to Lab 1
+
+*/
+
+
+const recipesFile =
 [
     {
       "recipeName": "Cheese Burger",
@@ -35,7 +51,7 @@
           "ingredientName": "cheese",
           "measure": "slices",
           "qty": "1"
-        },
+        }
         
       ]
     },
@@ -60,6 +76,25 @@
           "qty": "1"
         }
       ]
-    },
+    }
     
   ]
+
+  /* Insert Many */
+  db.recipes.insertMany(recipesFiles);
+
+/* query to get all recipes*/
+   db.recipes.find();
+
+   /* query to find particular ingredient */
+   db.recipes.find({"ingredients.ingredientName":"hamburger"});
+
+   /* update a recipe you added */
+
+   db.recipes.updateOne(
+   { recipeName: "Cheese Burger" },
+   {
+       $set: {"recipeName": "Grilled Cheese Burger"}
+   }
+   )
+   
